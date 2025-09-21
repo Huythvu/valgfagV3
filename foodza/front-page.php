c5c934a7e9d0d598dd3537d424df60f0e5f0af1f
Andy <andyandyvu@gmail.com>
2025-09-21 19:27:07 +0200

    rigtige billeder til frontpage

--- foodza/front-page.php
+++ foodza/front-page.php
@@ -10,14 +10,14 @@
   ?>
   <div class="front front-hero">
     <h1>Velkommen til FoodZa</h1>
-    <img src="<?php echo (get_theme_file_uri('/assets/images/errorCat.png')); ?>" alt="hero-image">
+    <img src="<?php echo (get_theme_file_uri('/assets/images/hero.png')); ?>" alt="hero-image">
     <p>Scroll, gem og del - Foodza samler madelskere, der brænder for gode opskrifter</p>
   </div>
 
   <div class="front front-recipe">
     <h2>Recent recipes</h2>
     <a href="<?php echo (get_post_type_archive_link('recipe')); ?>">
-      <img src="<?php echo (get_theme_file_uri('/assets/images/errorCat.png')); ?>" alt="Recipes">
+      <img src="<?php echo (get_theme_file_uri('/assets/images/recipe_food.png')); ?>" alt="Recipes">
     </a>
     <p>Opdag den nyeste opskrift - klar til at blive prøvet i dit køkken.</p>
   </div>
@@ -25,7 +25,7 @@
   <div class="front front-kitchen">
     <h2>Kitchen utensils</h2>
     <a href="<?php echo (get_post_type_archive_link('kitchenware')); ?>">
-      <img src="<?php echo (get_theme_file_uri('/assets/images/errorCat.png')); ?>" alt="Kitchenware">
+      <img src="<?php echo (get_theme_file_uri('/assets/images/kitchen_utensils.png')); ?>" alt="Kitchenware">
     </a>
     <p>Gør madlavningen nemmere med det rette køkkenudstyr.</p>
   </div>
@@ -33,7 +33,7 @@
   <div class="front front-community">
     <h2>See our latest community feed</h2>
     <a href="<?php echo (get_post_type_archive_link('communitypost')); ?>">
-      <img src="<?php echo (get_theme_file_uri('/assets/images/errorCat.png')); ?>" alt="Community">
+      <img src="<?php echo (get_theme_file_uri('/assets/images/community_happy.png')); ?>" alt="Community">
     </a>
     <p>Del dine opskrifter, få feedback og find nye køkkenvenner i vores community.</p>
   </div>
