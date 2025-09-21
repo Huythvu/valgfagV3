892bd57a991defd133f7e6df3aad13c432a05ed7
Huy Vu <10643808@ucn.dk>
2025-09-21 19:27:47 +0200

    community posts avatar to userprofile
    added site_url with get the author to get correct url for userprofile. (still unknown what happens if multiple people with same name.)

--- foodza/archive-communitypost.php
+++ foodza/archive-communitypost.php
@@ -3,43 +3,43 @@
     <article class="community-article">
         <h1>Create new post</h1>
         <input type="text" placeholder="What's on the menu today?" class="menu-textbox">
-        <button class="btn">Create post</button>
+        <button class="button communityBtn">Create post</button>
         </input>
         <section class="community-section">
             <?php while (have_posts()) {
               the_post();
               // the_title();
               ?>
-            <?php
-          $postImage = get_acpt_field([
-            'post_id'    => get_the_ID(),
-            'box_name'   => 'post-section',
-            'field_name' => 'post-image',
-          ]);
-          ?>
+
             <div class="community-content">
                 <div class="community-author">
-                    <span><?php echo get_avatar(get_the_author_meta('ID'), 50); ?></span>
+                    <a href="<?php echo site_url('/userprofile/' . get_the_author()); ?>"><?php echo get_avatar(get_the_author_meta('ID'), 50); ?>
                     <div class="community-author-date">
-                        <h2><?php echo get_the_author(); ?></h2>
-                        <p><?php echo get_the_date(); ?></p>
+                      <h2><?php echo get_the_author(); ?></h2>
+                    </a>
+                      <p><?php echo get_the_date(); ?></p>
                     </div>
                 </div>
                 <!-- Post Image, usually had esc_url for security, but didnt show img show removed -->
                 <div class="container-community">
+                    <?php
+                    $postImage = get_acpt_field([
+                    'post_id'    => get_the_ID(),
+                    'box_name'   => 'post-section',
+                    'field_name' => 'post-image',
+                    ]);
+                    ?>
                     <?php if ($postImage) { 
                 the_content();
                 ?>
                     <img class="recipe-img" src="<?php echo ($postImage->getSrc()); ?>"
                         alt="<?php echo ($postImage->getAlt()); ?>">
                     <?php } ?>
-                    </div>
-                    </div>
-                    <?php
+                </div>
+            </div>
+            <?php
             } ?>
-            </section>
+        </section>
     </article>
 </main>
 <?php get_footer(); ?>
-
-<img src="" alt="">
