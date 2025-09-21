54fe59d6e81b8defde6e5389cd043cbc866d79d4
Andy <andyandyvu@gmail.com>
2025-09-21 18:13:26 +0200

    tilføjese af front-page content og billeder

--- foodza/front-page.php
+++ foodza/front-page.php
@@ -2,12 +2,41 @@
 
 <main>
   <?php
-while (have_posts()) {
-  the_post();
-  the_title();
-  the_content();
-}
-?>
-</main> 
+  while (have_posts()) {
+    the_post();
+    the_title();
+    the_content();
+  }
+  ?>
+  <div class="front front-hero">
+    <h1>Velkommen til FoodZa</h1>
+    <img src="<?php echo (get_theme_file_uri('/assets/images/errorCat.png')); ?>" alt="hero-image">
+    <p>Scroll, gem og del - Foodza samler madelskere, der brænder for gode opskrifter</p>
+  </div>
+
+  <div class="front front-recipe">
+    <h2>Recent recipes</h2>
+    <a href="<?php echo (get_post_type_archive_link('recipe')); ?>">
+      <img src="<?php echo (get_theme_file_uri('/assets/images/errorCat.png')); ?>" alt="Recipes">
+    </a>
+    <p>Opdag den nyeste opskrift - klar til at blive prøvet i dit køkken.</p>
+  </div>
+
+  <div class="front front-kitchen">
+    <h2>Kitchen utensils</h2>
+    <a href="<?php echo (get_post_type_archive_link('kitchenware')); ?>">
+      <img src="<?php echo (get_theme_file_uri('/assets/images/errorCat.png')); ?>" alt="Kitchenware">
+    </a>
+    <p>Gør madlavningen nemmere med det rette køkkenudstyr.</p>
+  </div>
+
+  <div class="front front-community">
+    <h2>See our latest community feed</h2>
+    <a href="<?php echo (get_post_type_archive_link('communitypost')); ?>">
+      <img src="<?php echo (get_theme_file_uri('/assets/images/errorCat.png')); ?>" alt="Community">
+    </a>
+    <p>Del dine opskrifter, få feedback og find nye køkkenvenner i vores community.</p>
+  </div>
+</main>
 
 <?php get_footer(); ?>
