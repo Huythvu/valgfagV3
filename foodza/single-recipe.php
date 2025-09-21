ede35848e70e2fc536ad74dcbb1c4b18e81619b0
Andy <andyandyvu@gmail.com>
2025-09-22 01:20:45 +0200

    fanget metatags + enkelt recipe ID

--- foodza/single-recipe.php
+++ foodza/single-recipe.php
@@ -18,11 +18,20 @@ $recipeImage = get_acpt_field([
         <?php if ($recipeImage) { ?>
             <img src="<?php echo ($recipeImage->getSrc()); ?>" alt="<?php echo ($recipeImage->getAlt()); ?>">
         <?php } ?>
-        <p><?php the_content(); ?></p>
+        <div class="content-text">
+            <p><?php the_content(); ?></p>
+        </div>
     </div>
     <div class="recipe-text">
         <h1><?php the_title(); ?></h1>
-        <p>metatags</p>
+        <p><?php the_terms(get_the_ID(), 'cook-time', 'Cook-time:'); ?></p>
+        <p><?php the_terms(get_the_ID(), 'cook-level', 'Cook-level:'); ?></p>
+        <p><?php the_terms(get_the_ID(), 'dietary', 'Dietary:'); ?></p>
+        <p><?php the_terms(get_the_ID(), 'meal-type', 'Meal-type:'); ?></p>
+        <p><?php the_terms(get_the_ID(), 'tool', 'Tool:'); ?></p>
+
+        <!-- https://developer.wordpress.org/reference/functions/the_terms/?utm_source=chatgpt.com The_terms er en function i wordpress her kan jeg direkte fange mine taxonomier ved hjælp af the_terms også bare skrive category name jeg har angivet. -->
+
     </div>
 </div>
 
@@ -66,7 +75,7 @@ if ($instruction) {
 ?>
 <div class="related-recipes">
     <div class="related-recipes-text">
-        <a href="<?php echo (get_post_type_archive_link('recipe')); ?>">
+        <a href="<?php echo get_the_permalink(67); ?>">
             <img src="<?php echo (get_theme_file_uri('/assets/images/errorCat.png')); ?>" alt="Recipes">
             <p>brand</p>
             <p>navn</p>
@@ -74,7 +83,7 @@ if ($instruction) {
         </a>
     </div>
     <div class="related-recipes-text">
-        <a href="<?php echo (get_post_type_archive_link('recipe')); ?>">
+        <a href="<?php echo get_the_permalink(70); ?>">
             <img src="<?php echo (get_theme_file_uri('/assets/images/errorCat.png')); ?>" alt="Recipes">
             <p>brand</p>
             <p>navn</p>
@@ -82,7 +91,7 @@ if ($instruction) {
         </a>
     </div>
     <div class="related-recipes-text">
-        <a href="<?php echo (get_post_type_archive_link('recipe')); ?>">
+        <a href="<?php echo get_the_permalink(71); ?>">
             <img src="<?php echo (get_theme_file_uri('/assets/images/errorCat.png')); ?>" alt="Recipes">
             <p>brand</p>
             <p>navn</p>
