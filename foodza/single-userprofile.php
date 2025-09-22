e82d74aafe65fe8ec75fddc31a49e4e14508afff
Huy Vu <10643808@ucn.dk>
2025-09-22 02:03:03 +0200

    userprofile styling
    styling and added wp for userprofiles

--- foodza/single-userprofile.php
+++ foodza/single-userprofile.php
@@ -1,12 +1,11 @@
 <?php get_header(); ?>
-<main> 
-  <?php
+<main>
+    <?php
 while ( have_posts() ) {
   the_post();
-  the_title();
 }
 ?>
-<?php
+    <?php
 $userGender = get_acpt_field([
   'post_id'    => get_the_ID(),
   'box_name'   => 'user-tags',
@@ -22,36 +21,71 @@ $userLocation = get_acpt_field([
   'box_name'   => 'user-tags',
   'field_name' => 'location'
 ]);
+$userEducation = get_acpt_field([
+  'post_id'    => get_the_ID(),
+  'box_name'   => 'user-expertise',
+  'field_name' => 'user-education'
+]);
+$userExperience = get_acpt_field([
+  'post_id'    => get_the_ID(),
+  'box_name'   => 'user-expertise',
+  'field_name' => 'user-experience'
+]);
 ?>
 
-<?php
+    <?php
 if($userLocation){
 $userLocationString = $userLocation['country'] . ', ' . $userLocation['city'];
 } 
 ?>
 
-<?php
+    <?php
 if($userBirthday){
   $userBirthdayString = $userBirthday['object']->format('Y-m-d');
 } 
 ?>
-
-<ol>
-  <li class="user-display-name"><?php echo get_the_ID(); ?></li>
-  <li class="user-gender"><?php echo $userGender; ?></li>
-  <li class="user-birthday"><?php echo $userBirthday; ?></li>
-  <li class="user-location"><?php echo $userLocationString; ?></li>
-  <li class="user-account-creation"><?php echo get_the_date( 'Y-m-d', get_the_ID() ); ?></li>
-</ol>
-
-<?php 
-echo '<pre>';
-print_r(get_the_ID(127));
-print_r($userGender);
-print_r($userBirthday);
-print_r($userLocation);
-echo '</pre>';
-?>
+    <div class="userProfilePage">
+        <div class="userProfileContainer">
+            <div class="userTopContainer">
+                <div class="userTags">
+                    <span class="userProfileImg"><?php echo get_avatar(get_the_author_meta('ID'), 200); ?></span>
+                    <ul>
+                        <li class="user-display-name">Name: <?php echo get_the_author() ?></li>
+                        <li class="user-gender">Gender: <?php echo $userGender; ?></li>
+                        <li class="user-birthday">Birthday: <?php echo $userBirthdayString; ?></li>
+                        <li class="user-location">Location: <?php echo $userLocationString; ?></li>
+                        <li class="user-account-creation">Account Created:
+                            <?php echo get_the_date('d-m-Y', get_the_ID()); ?>
+                        </li>
+                    </ul>
+                </div>
+                <div class="userDescription">
+                    <div class="userDescriptionTopContent">
+                        <h2>About me</h2>
+                        <h2>Followers:</h2>
+                    </div>
+                    <hr>
+                    <div class="userDescriptionBottomContent">
+                        <p><?php echo get_the_content(); ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="userExpertise">
+                <?php if($userEducation){ ?>
+                <h2>My Education</h2>
+                <p><?php echo $userEducation ?></p>
+                <hr>
+                <?php } ?>
+                <?php if($userExperience){ ?>
+                <h2>My Experience</h2>
+                <p><?php echo $userExperience ?></p>
+                <?php } ?>
+            </div>
+            <div class="userRecipes">
+              
+            </div>
+        </div>
+    </div>
 
 </main>
 <?php get_footer(); ?>
